8789aa45b73e74b062d3aac26b2cf6b500d4e2a9
AndyKach <akwest6@gmail.com>
2025-01-11 21:10:54 +0100

    change app

--- src/infrastructure/config/fastapi_app_config.py
+++ src/infrastructure/config/fastapi_app_config.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from infrastructure.config import logs_config
 
@@ -20,6 +21,14 @@ async def lifespan(app: FastAPI):
     # app.include_router(router)
     yield
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def config():
     system_logger.info("Start uvicorn configuration")
